c3d28c65f7cb4a2c90b8f5d1d310c53902a45237
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-08-02 11:49:51 +0200

    add auth middleware + img converter into a string (imgUrl)

--- backend/routes/sauceRoutes.js
+++ backend/routes/sauceRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const sauceController = require('../controllers/sauceController');
+const auth = require('../middleware/auth');
 
-router.get('/', sauceController.displaySauces);         //Get all sauces
-router.get('/:id', sauceController.displayOneSauce);    //Get one sauce
-router.post('/', sauceController.createSauce);          //Post a new sauce
-router.put('/:id', sauceController.updateSauce);        //Update a sauce
-router.delete(':id', sauceController.deleteSauce);      //Delete sauce
+router.get('/', auth, sauceController.displaySauces);         //Get all sauces
+router.get('/:id', auth,sauceController.displayOneSauce);    //Get one sauce
+router.post('/', auth, sauceController.createSauce);          //Post a new sauce
+router.put('/:id', auth,sauceController.updateSauce);        //Update a sauce
+router.delete(':id', auth, sauceController.deleteSauce);      //Delete sauce
 //router.post('/like', sauceController.like)    //A définir
 
 module.exports = router;
