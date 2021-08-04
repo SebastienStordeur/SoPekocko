39b28d6219e7476a401dda93846d56fd45114ec3
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-08-04 14:56:33 +0200

    commented functions

--- backend/controllers/sauceController.js
+++ backend/controllers/sauceController.js
@@ -2,19 +2,24 @@ const Sauce = require('../models/sauceModel');
 const fs = require('fs');
 const User = require('../models/userModel');
 
+
+//Create a Sauce
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce ({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` //Reconstruction de la source de l'image (ici dossier images)
     //req.protocol = http or https           req.get('host') => nom d'hôte          $req.file.filename = nom du fichier
   });
+  //Save Sauce
   sauce.save()
     .then(() => res.status(201).json({ message: "Sauce créée"}))
-    .catch(error => res.status(400).json({ message: 'impossible de creer la sauce' }));
+    .catch(error => res.status(400).json({ message: 'impossible de créer la sauce' }));
 } 
 
+
+//Update a Sauce (only available for the creator) => See auth middleware
 exports.updateSauce = (req, res, next) => {
   const sauceObject = req.file ? 
   { 
@@ -26,12 +31,14 @@ exports.updateSauce = (req, res, next) => {
     .catch(error => res.status(404).json(error));
 }
 
+
+//Delete a sauce (only avaiblable for the creator) => See auth middleware
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
+  Sauce.findOne({ _id: req.params.id })                                         //display the selected item with its id
     .then(sauce => {
       const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
+      fs.unlink(`images/${filename}`, () => {                                   //To delete the stored img
+        Sauce.deleteOne({ _id: req.params.id }) 
           .then(() => res.status(200).json({ message: 'Sauce supprimée'}))
           .catch(error => res.status(400).json({ error }));
       })
@@ -39,18 +46,22 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 } 
 
+
+//Display all sauces once logged in
 exports.displaySauces = (req, res, next) => {
   Sauce.find()
     .then(sauces => res.status(200).json( sauces ))
     .catch(error => res.status(404).json({ error }));
 }
 
+//Display the clicked sauce with its infos (see model)
 exports.displayOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
+  Sauce.findOne({ _id: req.params.id })                                       //display the selected item with its id
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json(error));
 } 
 
+//Manage like and dislikes
 exports.like = async (req,res,next) => {
   try {
     await Sauce.findByIdAndUpdate(
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,51 +1,35 @@
 const User = require('../models/userModel'); //Import user model
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');         //Generate a token
+const bcrypt = require('bcrypt');            //Hash our password
 
 //signup
-
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 15)
-    .then(hash => {
+  bcrypt.hash(req.body.password, 15)                      //Hash function 
+    .then(hash => {                                         
       const user = new User({
         email: req.body.email,
         password: hash
       });
-      user.save()
+      user.save()                                         //Save in db
         .then(() => res.status(201).json({ message: "Utilisateur crée"}))
         .catch(error => res.status(500).json({ message: "Impossible de créer l'utilisateur" + error}));
     })
 }
 
 //login 
-
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
-      if(!user) return res.status(401).json({ message: "Impossible de trouver cet utilisateur"})
-      bcrypt.compare(req.body.password, user.password)
+      if(!user) return res.status(401).json({ message: "Impossible de trouver cet utilisateur"})                //If email isn't in the DB => error
+      bcrypt.compare(req.body.password, user.password)                                                          //Compare both passwords, the one from login and the one from db
         .then(valid => {
-          if(!valid) return res.status(401).json({ message: "Mot de passe incorrect"})
+          if(!valid) return res.status(401).json({ message: "Mot de passe incorrect"})                          //If psw isn't valid => error
           res.status(200).json({
             userId: user._id,
-            //token: "TOKEN",
-            token: jwt.sign({ userId: user._id }, "SECRET_TOKEN", { expiresIn: '24h' })
+            token: jwt.sign({ userId: user._id }, "SECRET_TOKEN", { expiresIn: '24h' })                         //Token available 24h
           });
         })
         .catch(error => res.status(500).json({ message:  "erreur1" }));
     })
     .catch(error => res.status(500).json({message: "erreur 2"}))
 }
-
-/* function authentificateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if(token == null) return res.sendStatus(401)
-
-  jwt.verify(token, "SECRET_TOKEN", (err, user) => {
-    if(err) return res.sendStatus(403) 
-    req.user = user 
-    next()
-  })
-} */
