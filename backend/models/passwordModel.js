b09e513eed60f39caf5a12734e88b0273a7af484
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-08-10 08:39:29 +0200

    add symbol requirement in passwords

--- backend/models/passwordModel.js
+++ backend/models/passwordModel.js
@@ -4,9 +4,10 @@ const pswSchema = new PasswordValidator;
 pswSchema 
 .is().min(8)
 .is().max(64)
-.has().uppercase(1)
-.has().lowercase(1)
-.has().digits(1)
+.has().uppercase()
+.has().lowercase()
+.has().digits()
+.has().symbols()
 .has().not().spaces()
 
 // Longueur entre 8 et 64 caractères, doit contenir une minuscule, une majuscule
